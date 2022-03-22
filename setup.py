261a67947415b6d57e20239448a441fe5065d90b
Allan Pichardo <allan.pichardo@gmail.com>
2022-03-21 21:38:41 -0700

    Created setup.py to install package with pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lightweight-gan',
+    version='',
+    packages=['lightweight_gan', 'lightweight_gan.layers', 'lightweight_gan.models'],
+    url='',
+    license='',
+    author='Allan Pichardo',
+    author_email='allan.pichardo@gmail.com',
+    description=''
+)
